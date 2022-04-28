14e4d40d6aff7c6ff8ff32af9086adf71e9bd71b
crisepu <csepulvedavenegas@gmail.com>
2022-04-28 16:54:30 +0000

    incorporado fetch funcional

--- src/js/component/setList.jsx
+++ src/js/component/setList.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 const SetList = (props) => {
-	const songList = props.songs.map((song, index) => {
-		return (
-			<div
-				key={index}
-				className="col list"
-				onClick={() => {
-					props.setCurrentSong(index);
-					props.setPlayFlag();
-				}}>
-				{song.id} - {song.name}
-			</div>
-		);
-	});
+	const songList =
+		!!props.songs &&
+		props.songs.map((song, index) => {
+			return (
+				<div
+					key={index}
+					className="col list"
+					onClick={() => {
+						props.setCurrentSong(index);
+						props.setPlayFlag();
+					}}>
+					{song.id} - {song.name}
+				</div>
+			);
+		});
 
 	return (
 		<div className="generalList">
